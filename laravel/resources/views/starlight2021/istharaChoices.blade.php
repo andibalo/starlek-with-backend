c197f28c856f2686a8fd157148cf512dfad34676
lifosmin <65651883+lifosmin@users.noreply.github.com>
2021-07-29 23:06:29 +0700

    frontend changes

--- laravel/resources/views/starlight2021/istharaChoices.blade.php
+++ laravel/resources/views/starlight2021/istharaChoices.blade.php
@@ -15,19 +15,23 @@
 @endsection
 
 @section('content')
-    <div class="section-1 d-flex">
-        <div class="row">
-            <div class="col-12">
-                <h1>Are you entering as:</h1>
+    <div class="section-1 d-flex flex-column">
+        <div class="pt-5 my-5">
+            <div class="row d-flex justify-content-center pt-5 mt-3">
+                <div class="col-6 p-0 position-relative">
+                    <h3 class="choice-text">Solo</h3>
+                    <a href="/contestant/solo"><img src="https://images.unsplash.com/photo-1615789591457-74a63395c990?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt=""></a>
+                </div>
+                <div class="col-6 p-0">
+                    <h3 class="choice-text">Group</h3>
+                    <a href="/contestant/group"><img src="https://images.unsplash.com/photo-1615789591457-74a63395c990?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt=""></a>
+                </div>
             </div>
-            <div class="col-6">
-                <a href="/contestant/solo">Solo</a>
-            </div>
-            <div class="col-6">
-                <a href="/contestant/group">Group</a>
+            <div class="row d-flex justify-content-center p-3">
+                <h4 class="terms-text">Terms & regulation</h2>
             </div>
         </div>
-
+       
     </div>
     
     @endsection
--- laravel/resources/views/starlight2021/istharaGroup.blade.php
+++ laravel/resources/views/starlight2021/istharaGroup.blade.php
@@ -30,33 +30,80 @@
                 </ul>
                 <!-- fieldsets -->
                 <fieldset>
-                    <h2 class="fs-title">Lot Details</h2>
-                    <h3 class="fs-subtitle"></h3>
-                    <input type="text" id="link-video" name="link-video" placeholder="Number of Spaces"/>
-                    <input type="text" id="stage-name" name="stage-name" placeholder="Price of parking"/>
-                    <input type="text" id="talent" name="talent" placeholder="Avg. number of cash payments"/>
-                    <input type="button" name="next" class="next action-button" value="Next"/>
+                    <div class="row ms-form-wrapper">
+                        <div class="col-4">
+                            <h2 class="fs-title">Welcome Starlighters</h2>
+                            <h3 class="fs-subtitle"></h3>
+                        </div>
+                        <div class="col-8">
+                            <label for="link-video d-flex">Video Link</label>
+                            <input type="text" id="link-video" name="link-video" placeholder="https://www.instagram.com/starlightumn/"/>
+                            <label for="link-video d-flex">Stage Name</label>
+                            <input type="text" id="stage-name" name="stage-name" placeholder="Insert your stage name here"/>
+                            <label for="link-video d-flex">Talent Description</label>
+                            <input type="text" id="talent" name="talent" placeholder="Describe your talent briefly"/>
+                            <div class="next-container d-flex justify-content-end">
+                                <input type="button" name="next" class="next action-button" value="Next"/>
+                            </div>
+                            
+                        </div>
+                    </div>                         
+                    
                 </fieldset>
                 <fieldset>
-                    <h2 class="fs-title">Event Operations</h2>
-                    <h3 class="fs-subtitle"></h3>
-                    <input type="text" name="line-id" id="line-id" placeholder="Events per year"/>
-                    <input type="number" name="phone-number" id="phone-number" placeholder="Lot attendants per event"/>
-                    <input type="text" name="instagram" id="instagram" placeholder="Attendant hourly rate"/>
-                    <input type="text" name="e-mail" id="e-mail" placeholder="Labor hours per event"/>
-                    <input type="number" name="num-participants" id="num-participants" placeholder="Labor hours per event"/>
-                    <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                    <input type="button" name="next" class="next action-button" value="Next"/>
+                    <div class="row ms-form-wrapper">
+                        <div class="col-4 d-flex flex-column justify-content-between">
+                            <h2 class="fs-title">Welcome Starlighters</h2>
+                            <h3 class="fs-subtitle"></h3>
+                            <div class="next-container d-flex justify-content-start">
+                                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <label for="link-video d-flex">LINE ID (representative)</label>
+                            <input type="text" name="line-id" id="line-id" placeholder="@starlightumn"/>
+                            <label for="link-video d-flex">Phone Number (representative)</label>
+                            <input type="number" name="phone-number" id="phone-number" placeholder="+6281122334455"/>
+                            <label for="link-video d-flex">Instagram Username</label>
+                            <input type="text" name="instagram" id="instagram" placeholder="@starlightumn"/>
+                            <label for="link-video d-flex">E-mail Address</label>
+                            <input type="text" name="e-mail" id="e-mail" placeholder="starlight@umn.ac.id"/>
+                            <label for="link-video d-flex">Number of Performers</label>
+                            <input type="number" name="num-participants" id="num-participants" placeholder="0"/>
+                            
+                            <div class="next-container d-flex justify-content-end">
+                                    <input type="button" name="next" class="next action-button" value="Next"/>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </fieldset>
                 <fieldset>
-                    <h2 class="fs-title">Additional Information</h2>
-                    <h3 class="fs-subtitle">Fill in your credentials to get your ROI report</h3>
-                    <input type="text" id="full-name" name="name" placeholder="Name"/>
-                    <input type="date" name="date-birth" id="date-birth" placeholder="Copmany"/>
-                    <input type="text" name="address" id="address" placeholder="Copmany"/>
-                    <input type="text" name="education" id="education" placeholder="Copmany"/>
-                    <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                    <input type="submit" name="submit" id="submitBtn" class="submit" value="Submit"/>
+                    <div class="row ms-form-wrapper">
+                        <div class="col-4 d-flex flex-column justify-content-between">
+                            <h2 class="fs-title">Welcome Starlighters</h2>
+                            <h3 class="fs-subtitle"></h3>
+                            <div class="next-container d-flex justify-content-start">
+                                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <label for="link-video d-flex">Full name</label>
+                            <input type="text" id="full-name" name="name" placeholder="Insert your full name here"/>
+                            <label for="link-video d-flex">Date of Birth</label>
+                            <input type="date" name="date-birth" id="date-birth" placeholder="2021-08-21"/>
+                            <label for="link-video d-flex">Address</label>
+                            <input type="text" name="address" id="address" placeholder="Jl. Scientia Boulevard, Gading, Kec. Serpong, Tangerang, Banten 15227"/>
+                            <label for="link-video d-flex">Educational Institution</label>
+                            <input type="text" name="education" id="education" placeholder="Universitas Multimedia Nusantara"/>
+                            <label for="link-video d-flex">Scan Identity Card (KTP/KITAS/Kartu Pelajar/KIA)</label>
+                            <input type="file" name="education" accept="image/png, image/jpeg, image/jpg" id="education" placeholder="Universitas Multimedia Nusantara"/>
+                            <div class="next-container d-flex justify-content-end">
+                                <input type="submit" name="submit" id="submitBtn" class="submit" value="Submit"/>
+                            </div>
+                        </div> 
+                    </div>                          
+                    
                 </fieldset>
             </form>
             <!-- link to designify.me code snippets -->
@@ -81,69 +128,60 @@
 
 
             $(".next").click(function() {
-                var talent = document.getElementById("talent").value;
-                var instagram = document.getElementById("instagram").value;
-                if(x == "" && y ==""){
-                    document.getElementById("talent").style.borderColor="red";
-                    console.log(x);
-                }
-                else {
-                    console.log(x);
-                    if (animating) return false;
-                    animating = true;
+                if (animating) return false;
+                animating = true;
 
-                    current_fs = $(this).parent();
-                    next_fs = $(this)
-                        .parent()
-                        .next();
+                current_fs = $(this).closest("fieldset");
+                next_fs = $(this)
+                    .closest("fieldset")
+                    .next();
 
-                    //activate next step on progressbar using the index of next_fs
-                    $("#progressbar li")
-                        .eq($("fieldset").index(next_fs))
-                        .addClass("active");
+                //activate next step on progressbar using the index of next_fs
+                $("#progressbar li")
+                    .eq($("fieldset").index(next_fs))
+                    .addClass("active");
 
-                    //show the next fieldset
-                    next_fs.show();
-                    //hide the current fieldset with style
-                    current_fs.animate(
-                        { opacity: 0 },
-                        {
-                        step: function(now, mx) {
-                            //as the opacity of current_fs reduces to 0 - stored in "now"
-                            //1. scale current_fs down to 80%
-                            scale = 1 - (1 - now) * 0.2;
-                            //2. bring next_fs from the right(50%)
-                            left = now * 50 + "%";
-                            //3. increase opacity of next_fs to 1 as it moves in
-                            opacity = 1 - now;
-                            current_fs.css({
-                                transform: "scale(" + scale + ")",
-                                position: "absolute"
-                            });
-                            next_fs.css({ 
-                                // left: left,
-                                opacity: opacity 
-                            });
-                        },
-                        duration: 800,
-                        complete: function() {
-                            current_fs.hide();
-                            animating = false;
-                        },
-                        //this comes from the custom easing plugin
-                        easing: "easeInOutBack"
-                        }
-                    );
-                }
+                //show the next fieldset
+                next_fs.show();
+                //hide the current fieldset with style
+                current_fs.animate(
+                    { opacity: 0 },
+                    {
+                    step: function(now, mx) {
+                        //as the opacity of current_fs reduces to 0 - stored in "now"
+                        //1. scale current_fs down to 80%
+                        scale = 1 - (1 - now) * 0.2;
+                        //2. bring next_fs from the right(50%)
+                        left = now * 50 + "%";
+                        //3. increase opacity of next_fs to 1 as it moves in
+                        opacity = 1 - now;
+                        current_fs.css({
+                            transform: "scale(" + scale + ")",
+                            position: "absolute"
+                        });
+                        next_fs.css({ 
+                            // left: left,
+                            opacity: opacity 
+                        });
+                    },
+                    duration: 800,
+                    complete: function() {
+                        current_fs.hide();
+                        animating = false;
+                    },
+                    //this comes from the custom easing plugin
+                    easing: "easeInOutBack"
+                    }
+                );
             });
 
             $(".previous").click(function() {
             if (animating) return false;
             animating = true;
 
-            current_fs = $(this).parent();
+            current_fs = $(this).closest("fieldset");
             previous_fs = $(this)
-                .parent()
+                .closest("fieldset")
                 .prev();
 
             //de-activate current step on progressbar
